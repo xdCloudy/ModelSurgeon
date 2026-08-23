c03131a992c865094cbf90e903c2de6793563c82
xdCloudy <xdCloudy@users.noreply.github.com>
2026-08-23 14:07:26 +0100

    feat: define model adapter capability contract

--- src/modelsurgeon/adapters/__init__.py
+++ src/modelsurgeon/adapters/__init__.py
@@ -1,2 +1,34 @@
-"""Model format adapters."""
+"""Framework-neutral model format adapter contracts."""
+
+from modelsurgeon.adapters.base import (
+    AdapterCapability,
+    AdapterIdentity,
+    ComponentDescriptor,
+    ModelAdapter,
+    ModelFormat,
+    ModelSession,
+    ModelSource,
+    MutationSupport,
+    OpenOptions,
+    TensorChunk,
+    TensorDescriptor,
+    UnsupportedCapabilityError,
+    require_capability,
+)
+
+__all__ = [
+    "AdapterCapability",
+    "AdapterIdentity",
+    "ComponentDescriptor",
+    "ModelAdapter",
+    "ModelFormat",
+    "ModelSession",
+    "ModelSource",
+    "MutationSupport",
+    "OpenOptions",
+    "TensorChunk",
+    "TensorDescriptor",
+    "UnsupportedCapabilityError",
+    "require_capability",
+]
 
